7d52af7ed85ccd2d3792570c4ba6aac544cc40a8
Randolph-zeng <randolphzeng@gmail.com>
2024-07-23 15:10:57 +0800

    refactor: move all the prompts into specific file

---
+++ agentless/construct/prompts.py
@@ -0,0 +1,320 @@
+obtain_relevant_files_prompt = """
+Please look through the following GitHub problem description and Repository structure and provide a list of files that one would need to edit to fix the problem.
+
+### GitHub Problem Description ###
+{problem_statement}
+
+###
+
+### Repository Structure ###
+{structure}
+
+###
+
+Please only provide the full path and return at most 5 files.
+The returned files should be separated by new lines ordered by most to least important and wrapped with ```
+For example:
+```
+file1.py
+file2.py
+```
+"""
+
+    
+obtain_relevant_code_prompt = """
+Please look through the following GitHub problem description and file and provide a set of locations that one would need to edit to fix the problem.
+
+### GitHub Problem Description ###
+{problem_statement}
+
+###
+
+### File: {file_name} ###
+{file_content}
+
+###
+
+Please provide either the class, the function name or line numbers that need to be edited.
+### Example 1:
+```
+class: MyClass
+```
+### Example 2:
+```
+function: my_function
+```
+### Example 3:
+```
+line: 10
+line: 24
+```
+
+Return just the location(s)
+"""
+
+
+file_content_template = """
+### File: {file_name} ###
+{file_content}
+"""
+
+
+file_content_in_block_template = """
+### File: {file_name} ###
+```python
+{file_content}
+```
+"""
+
+
+obtain_relevant_code_combine_top_n_prompt = """
+Please review the following GitHub problem description and relevant files, and provide a set of locations that need to be edited to fix the issue.
+The locations can be specified as class names, function or method names, or exact line numbers that require modification.
+
+### GitHub Problem Description ###
+{problem_statement}
+
+###
+{file_contents}
+
+###
+
+Please provide the class name, function or method name, or the exact line numbers that need to be edited.
+### Examples:
+```
+full_path1/file1.py
+line: 10
+class: MyClass1
+line: 51
+
+full_path2/file2.py
+function: MyClass2.my_method
+line: 12
+
+full_path3/file3.py
+function: my_function
+line: 24
+line: 156
+```
+
+Return just the location(s)
+"""
+
+
+obtain_relevant_code_combine_top_n_no_line_number_prompt = """
+Please review the following GitHub problem description and relevant files, and provide a set of locations that need to be edited to fix the issue.
+The locations can be specified as class, method, or function names that require modification.
+
+### GitHub Problem Description ###
+{problem_statement}
+
+###
+{file_contents}
+
+###
+
+Please provide the class, method, or function names that need to be edited.
+### Examples:
+```
+full_path1/file1.py
+function: my_function1
+class: MyClass1
+
+full_path2/file2.py
+function: MyClass2.my_method
+class: MyClass3
+
+full_path3/file3.py
+function: my_function2
+```
+
+Return just the location(s)
+"""
+
+
+obtain_relevant_functions_from_compressed_files_prompt = """
+Please look through the following GitHub problem description and the skeleton of relevant files.
+Provide a thorough set of locations that need inspection or editing to fix the problem, including directly related areas as well as any potentially related functions and classes.
+
+### GitHub Problem Description ###
+{problem_statement}
+
+###
+{file_contents}
+
+###
+
+Please provide locations as either the class or the function name.
+### Examples:
+```
+full_path1/file1.py
+class: MyClass1
+
+full_path2/file2.py
+function: MyClass2.my_method
+
+full_path3/file3.py
+function: my_function
+```
+
+Return just the location(s)
+"""
+
+
+obtain_relevant_functions_and_vars_from_compressed_files_prompt_more = """
+Please look through the following GitHub Problem Description and the Skeleton of Relevant Files.
+Identify all locations that need inspection or editing to fix the problem, including directly related areas as well as any potentially related global variables, functions, and classes.
+For each location you provide, either give the name of the class, the name of a method in a class, the name of a function, or the name of a global variable.
+
+### GitHub Problem Description ###
+{problem_statement}
+
+### Skeleton of Relevant Files ###
+{file_contents}
+
+###
+
+Please provide the complete set of locations as either a class name, a function name, or a variable name.
+Note that if you include a class, you do not need to list its specific methods.
+You can include either the entire class or don't include the class name and instead include specific methods in the class.
+### Examples:
+```
+full_path1/file1.py
+function: my_function_1
+class: MyClass1
+function: MyClass2.my_method
+
+full_path2/file2.py
+variable: my_var
+function: MyClass3.my_method
+
+full_path3/file3.py
+function: my_function_2
+function: my_function_3
+function: MyClass4.my_method_1
+class: MyClass5
+```
+
+Return just the locations.
+"""
+
+
+repair_relevant_file_instruction = """
+Below are some code segments, each from a relevant file. One or more of these files may contain bugs.
+"""
+
+
+repair_relevant_file_with_scope_instruction = """
+Below are some code segments, each from a relevant file. One or more of these files may contain bugs.
+In the file below, "..." refers to some less relevant content being omited for brebity.
+"""
+
+
+with_scope_explanation = """
+Note that "..." refers to some omited content that is not actually in the files. Your *SEARCH/REPLACE* edit must not contain such "...".
+"""
+
+
+repair_relevant_file_with_suspicious_loc_instruction = """
+Below are some code segments, each from a relevant file. One or more of these files may contain bugs. Some suspicious locations are provided for closer inspection.
+"""
+
+
+repair_prompt_combine_topn = """
+We are currently solving the following issue within our repository. Here is the issue text:
+--- BEGIN ISSUE ---
+{problem_statement}
+--- END ISSUE ---
+
+{repair_relevant_file_instruction}
+--- BEGIN FILE ---
+```
+{content}
+```
+--- END FILE ---
+
+Please generate `edit_file` commands to fix the issue.
+
+The `edit_file` command takes four arguments:
+
+edit_file(filename: str, start: int, end: int, content: str) -> None:
+    Edit a file. It replaces lines `start` through `end` (inclusive) with the given text `content` in the open file.
+    Args:
+    filename: str: The full file name to edit.
+    start: int: The start line number. Must satisfy start >= 1.
+    end: int: The end line number. Must satisfy start <= end <= number of lines in the file.
+    content: str: The content to replace the lines with.
+
+Please note that THE `edit_file` FUNCTION REQUIRES PROPER INDENTATION. If you would like to add the line '        print(x)', you must fully write that out, with all those spaces before the code!
+Wrap the `edit_file` command in blocks ```python...```.
+"""
+
+
+repair_prompt_combine_topn_cot = """
+We are currently solving the following issue within our repository. Here is the issue text:
+--- BEGIN ISSUE ---
+{problem_statement}
+--- END ISSUE ---
+
+{repair_relevant_file_instruction}
+--- BEGIN FILE ---
+```
+{content}
+```
+--- END FILE ---
+
+Please first localize the bug based on the issue statement, and then generate `edit_file` commands to fix the issue.
+
+The `edit_file` command takes four arguments:
+
+edit_file(filename: str, start: int, end: int, content: str) -> None:
+    Edit a file. It replaces lines `start` through `end` (inclusive) with the given text `content` in the open file.
+    Args:
+    filename: str: The full file name to edit.
+    start: int: The start line number. Must satisfy start >= 1.
+    end: int: The end line number. Must satisfy start <= end <= number of lines in the file.
+    content: str: The content to replace the lines with.
+
+Please note that THE `edit_file` FUNCTION REQUIRES PROPER INDENTATION. If you would like to add the line '        print(x)', you must fully write that out, with all those spaces before the code!
+Wrap the `edit_file` command in blocks ```python...```.
+"""
+
+
+repair_prompt_combine_topn_cot_diff = """
+We are currently solving the following issue within our repository. Here is the issue text:
+--- BEGIN ISSUE ---
+{problem_statement}
+--- END ISSUE ---
+
+{repair_relevant_file_instruction}
+--- BEGIN FILE ---
+```
+{content}
+```
+--- END FILE ---
+
+Please first localize the bug based on the issue statement, and then generate *SEARCH/REPLACE* edits to fix the issue.
+
+Every *SEARCH/REPLACE* edit must use this format:
+1. The file path
+2. The start of search block: <<<<<<< SEARCH
+3. A contiguous chunk of lines to search for in the existing source code
+4. The dividing line: =======
+5. The lines to replace into the source code
+6. The end of the replace block: >>>>>>> REPLACE
+
+Here is an example:
+
+```python
+### mathweb/flask/app.py
+<<<<<<< SEARCH
+from flask import Flask
+=======
+import math
+from flask import Flask
+>>>>>>> REPLACE
+```
+
+Please note that the *SEARCH/REPLACE* edit REQUIRES PROPER INDENTATION. If you would like to add the line '        print(x)', you must fully write that out, with all those spaces before the code!
+Wrap the *SEARCH/REPLACE* edit in blocks ```python...```.
+"""
